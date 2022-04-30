f942a5167fa39e437b05aa6f986f864333c32256
Cyrus Chan <cyruschan1212@gmail.com>
2022-04-30 14:00:52 +0800

    Both rate limit header enable cause conflict

--- api/ratelimit.js
+++ api/ratelimit.js
@@ -3,8 +3,6 @@ const rateLimit = require("express-rate-limit");
 module.exports = (second = 1, reqmax = 100) => rateLimit({
     windowMs: second * 1000,
     max: reqmax,
-    legacyHeaders: true,
-    standardHeaders: true,
     skip: (req, res) => req.ip === "127.0.0.1" || res.status === 301,
     message: {
         error: "Rate limit reached"
