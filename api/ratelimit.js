31af27554bd6d3cc28987473d80a7450790663f5
Cyrus Chan <cyruschan1212@gmail.com>
2022-06-09 11:57:23 +0800

    Don't count when origin is same website

--- api/ratelimit.js
+++ api/ratelimit.js
@@ -4,7 +4,7 @@ module.exports = function (second, maxreq) {
     return rateLimit({
         windowMs: second * 1000,
         max: maxreq,
-        skip: (req, res) => req.ip === "127.0.0.1" || res.status === 301 || req.method.toUpperCase() === "HEAD",
+        skip: (req, res) => req.ip === "127.0.0.1" || res.status === 301 || req.method.toUpperCase() === "HEAD" || /\.rk0cc\.xyz/gi.test(req.origin || ""),
         message: {
             error: "Rate limit reached"
         }
